e33a254948632f80fe5528c12207a13caa3f1929
Binary <binary@jrudevels.org>
2014-01-04 21:00:23 +0700

    load jslix from github

--- jslix-jingle/require-config.js
+++ jslix-jingle/require-config.js
@@ -2,8 +2,8 @@ require.config({
     baseUrl: 'js',
     paths: {
         'libs/jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min',
-        //'jslix': 'https://raw.github.com/jbinary/jslix/master/src/',
-        'jslix': 'http://dev.habahaba.im/js/libs/jslix',
+        'jslix': 'https://raw.github.com/jbinary/jslix/master/src/',
+        //'jslix': 'http://dev.habahaba.im/js/libs/jslix',
         'libs': 'libs',
         'cryptojs': 'http://crypto-js.googlecode.com/svn/tags/3.1.2/src/',
         'contextmenu': 'libs/jQuery-contextmenu'
